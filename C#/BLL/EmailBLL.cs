210c868c5f2fbe5b1bc18499a7a83f18e4226589
ayalay062 <ayalay062@gmail.com>
2021-11-18 12:45:30 +0200

    Ayala - Email

--- C#/BLL/EmailBLL.cs
+++ C#/BLL/EmailBLL.cs
@@ -4,68 +4,54 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Mail;
-
-
 using DAL;
 using DTO;
-namespace BLL
-{
-    public class EmailModel
-    {
+namespace BLL {
+    public class EmailModel {
         public bool isResetPassword { get; set; }
         public string toname { get; set; }
         public string toemail { get; set; }
         public string subject { get; set; }
         public string message { get; set; }
     }
-    public class EmailBLL
-    {
-        public static bool SendEmail(EmailModel objData)
-        {
-            MailMessage mail = new MailMessage();
-            if (objData.isResetPassword)
-            {
+    public class EmailBLL {
+        public static bool SendEmail (EmailModel objData) {
+            MailMessage mail = new MailMessage ();
+
+            if (objData.isResetPassword) {
                 var pass = "";
-                using (var db = new KeyMoneyEntities())
-                {
-                    var user = db.User.FirstOrDefault(d => d.email == objData.toemail);
+                using (var db = new KeyMoneyEntities ()) {
+                    var user = db.User.FirstOrDefault (d => d.email == objData.toemail);
                     if (user != null)
                         pass = user.id_user;
                 }
-                if (pass == "")
-                {
+                if (pass == "") {
                     return false;
                 }
-      
-                mail.Body = createEmailPassBody(pass, objData.message);
+
+                mail.Body = createEmailPassBody (pass, objData.message);
+
+            } else {
+                mail.To.Add ("keymoney2021.project@gmail.com");
+                mail.Body = createEmailBody (objData.toname, objData.message);
 
             }
-            else
-            {
-
-                mail.Body = createEmailBody(objData.toname, objData.message);
-                
-            }
-            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
-            mail.From = new MailAddress("keymoney2021.project@gmail.com");
-            mail.To.Add(objData.toemail);
+            SmtpClient SmtpServer = new SmtpClient ("smtp.gmail.com");
+            mail.From = new MailAddress ("keymoney2021.project@gmail.com");
+            mail.To.Add (objData.toemail);
+          
             mail.Subject = objData.subject;
             mail.IsBodyHtml = true;
             SmtpServer.UseDefaultCredentials = false;
             SmtpServer.Port = 587;
-            SmtpServer.Credentials = new System.Net.NetworkCredential("keymoney2021.project@gmail.com", "KM123456");
+            SmtpServer.Credentials = new System.Net.NetworkCredential ("keymoney2021.project@gmail.com", "KM123456");
             SmtpServer.EnableSsl = true;
 
-            SmtpServer.Send(mail);
+            SmtpServer.Send (mail);
             return true;
         }
 
-
-
-
-
-        private static string createEmailBody(string userName, string message)
-        {
+        private static string createEmailBody (string userName, string message) {
             var body = "<div>";
             if (userName != null)
                 body += "<h3>הי  " + userName + "</h3>";
@@ -74,8 +60,7 @@ namespace BLL
             return body;
         }
 
-        private static string createEmailPassBody(string pass, string message)
-        {
+        private static string createEmailPassBody (string pass, string message) {
             var body = "<div>";
 
             body += "<span>" + message + "<strong>" + pass + "</strong></span>";
